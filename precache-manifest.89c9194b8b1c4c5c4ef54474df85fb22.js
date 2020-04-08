3dd16040153bfd90603e68571248b62c3414dbe8
Taresh Sharma <taresh.sharma@synapse.com>
2020-04-09 01:35:14 +0530

    Updates

---
+++ precache-manifest.89c9194b8b1c4c5c4ef54474df85fb22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe3318b158905413282ed05316d2930",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/index.html"
+  },
+  {
+    "revision": "52563723d5923b0703ab",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8338386402060b4bc38b",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/css/main.f2b4194b.chunk.css"
+  },
+  {
+    "revision": "52563723d5923b0703ab",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/js/2.24593f00.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/js/2.24593f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8338386402060b4bc38b",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/js/main.7191055c.chunk.js"
+  },
+  {
+    "revision": "ef88dc65b47d14278c14",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/js/runtime-main.1baf356d.js"
+  },
+  {
+    "revision": "ad98d6e9cf499818f8814bfe6ee89ea7",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/media/gutten.ad98d6e9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taresh-sharma10.github.io/taresh-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
